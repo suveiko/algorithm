47e13949d7c1d75fbd214bb83c6cc7e20edb5637
suveiko <suveikosasha@gmail.com>
2022-06-01 13:35:02 +0400

    Selection sort

---
+++ 3_selection_sort.js
@@ -0,0 +1,22 @@
+const arr = [1, 4, 5, 77, 10, 15, 21, 17, 2, 7, 8, 10, -2]
+let count = 0
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let indexMin = i
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[indexMin]) {
+                indexMin = j
+            }
+            count += 1
+        }
+        let tmp = arr[i]
+        arr[i] = arr[indexMin]
+        arr[indexMin] = tmp
+    }
+    return arr
+}
+
+console.log(selectionSort(arr))
+console.log(arr.length)
+console.log(`count = ${count}`)
