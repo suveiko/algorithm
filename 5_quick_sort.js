3e4111bf34a87de73893bf7edbaac4767da532c7
suveiko <suveikosasha@gmail.com>
2022-06-06 15:47:50 +0400

    Quick sort

---
+++ 5_quick_sort.js
@@ -0,0 +1,27 @@
+const arr = [1, 4, 5, 77, 10, 15, 21, 17, 2, 7, 8, 10, -2, -5]
+let count = 0
+
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr
+    }
+    let pivotIndex = Math.floor(arr.length / 2)
+    let pivot = arr[pivotIndex]
+    let less = []
+    let greater = []
+    for (let i = 0; i < arr.length; i++) {
+        count += 1
+        if (i === pivotIndex) {
+            continue
+        }
+        if (arr[i] < pivot) {
+            less.push(arr[i])
+        } else {
+            greater.push(arr[i])
+        }
+    }
+    return [...quickSort(less), pivot, ...quickSort(greater)]
+}
+
+console.log(quickSort(arr))
+console.log(`count = ${count}`)
